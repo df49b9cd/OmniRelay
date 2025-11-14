ce3c0e44b1444ffdc22e3a8c8d825d6d772188c1
Soeren Magnus Olesen <soeren@molesen.ch>
2025-11-14 14:51:25 +0100

    refactor: Change IList to List for HttpUrls and GrpcUrls properties in DiagnosticsControlPlaneConfiguration
    Signed-off-by: Soeren Magnus Olesen <soeren@molesen.ch>

--- src/OmniRelay.Configuration/Models/DiagnosticsControlPlaneConfiguration.cs
+++ src/OmniRelay.Configuration/Models/DiagnosticsControlPlaneConfiguration.cs
@@ -4,14 +4,14 @@ namespace OmniRelay.Configuration.Models;
 public sealed class DiagnosticsControlPlaneConfiguration
 {
     /// <summary>HTTP control-plane bindings. Defaults to the classic dispatcher diagnostics endpoint.</summary>
-    public IList<string> HttpUrls { get; } = ["http://127.0.0.1:8080"];
+    public List<string> HttpUrls { get; set; } = ["http://127.0.0.1:8080"];
 
     public HttpServerRuntimeConfiguration HttpRuntime { get; init; } = new();
 
     public HttpServerTlsConfiguration HttpTls { get; init; } = new();
 
     /// <summary>gRPC control-plane bindings. Defaults to disabled until configured.</summary>
-    public IList<string> GrpcUrls { get; } = [];
+    public List<string> GrpcUrls { get; set; } = [];
 
     public GrpcServerRuntimeConfiguration GrpcRuntime { get; init; } = new();
 
